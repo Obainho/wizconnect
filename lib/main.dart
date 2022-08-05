f172f7e2675566dc2e62dda8844911bf9468f6aa
Uche Obinna <102373420+Obainho@users.noreply.github.com>
2022-08-05 10:02:40 +0100

    Add files via upload

---
+++ lib/main.dart
@@ -0,0 +1,90 @@
+import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  _launchURL(String url) async {
+    final Uri urlink = Uri.parse(url);
+    if (!await canLaunchUrl(urlink)) {
+      //Null check is vital in this line
+      await launchUrl(urlink, mode: LaunchMode.inAppWebView);
+    } else {
+      throw 'Could not launch $urlink';
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(30.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image.asset('assets/wizconnect.JPG'),
+              const Text(
+                "Welcome on board",
+                style: TextStyle(
+                  fontSize: 30,
+                  fontWeight: FontWeight.w400,
+                ),
+              ),
+              const Text(
+                "Lets get you connected...",
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.w500,
+                  color: Colors.deepOrange,
+                ),
+              ),
+              const SizedBox(height: 50),
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  primary: const Color.fromARGB(255, 56, 42, 255),
+                  elevation: 10,
+                ),
+                onPressed: () {
+                  const url = 'https://pub.dev';
+                  _launchURL(url);
+                },
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: const [
+                    Text(
+                      'Click to continue',
+                      style: TextStyle(
+                        color: Colors.white,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
